f60e81fbe1ca59cd4d861bf5d77c9bcb6fe4356e
fedecris <federicocristina@gmail.com>
2019-05-08 19:19:43 -0300

    Implementacion base para cambio de escenas (Exterior / Interior)

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (character.transform.position, transform.position) < 0.4f) {
+			string newCurrent = null;
+			if (SceneManager.GetActiveScene().name == "Exterior") {
+				newCurrent = "Interior";
+			} else {
+				newCurrent = "Exterior";
+			}
+			SceneManager.LoadScene (newCurrent);
+		}
+	}
+
+
+}
