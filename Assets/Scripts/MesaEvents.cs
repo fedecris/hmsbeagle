1bf9aca56e0d7be0efba8b80110b0b66f20fb95d
sdapoto <sdapoto@users.noreply.github.com>
2019-05-14 17:55:01 -0300

    GVR 1ra version
    Ambas escenas (interior y exterior) con funcionalidad y PlayerVR. Se mueve al clickear. Al clickear sobre la puerta switchea entre escenas

---
+++ Assets/Scripts/MesaEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MesaEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void showMessage()	{
+		//codigo para mostrar mensaje en pantalla;
+        Debug.Log("PRESIONO SOBRE LA MESA");
+	}
+	
+}
---
+++ Assets/Scripts/PuertaEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuertaEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Entrar al camarote
+    public void entrar()
+    {
+        SceneManager.LoadScene("Interior");
+    }
+    
+    // Salir a la cubierta
+    public void salir()
+    {
+        SceneManager.LoadScene("Exterior");
+    }	
+
+}
