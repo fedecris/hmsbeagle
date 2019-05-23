d856f47fc5653eb1d3aed643d2fa691b8049eb0c
fedecris <federicocristina@gmail.com>
2019-05-23 19:34:44 -0300

    Redefinicion de boundaries y definicion de textos

---
+++ Assets/Scripts/DisplayEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayEvents : MonoBehaviour {
+
+	public TextMesh objectText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void showMessage()	{
+		// muestro el texto
+		objectText.GetComponentInChildren<Renderer>().enabled = true;
+		//codigo para mostrar mensaje en pantalla;
+
+		Debug.Log("SHOW <TEXTO>");
+	}
+
+	public void unshowMessage()	{
+		// muestro el texto
+		objectText.GetComponentInChildren<Renderer>().enabled = false;
+
+		Debug.Log("UNSHOW <TEXTO>");
+	}
+
+}
